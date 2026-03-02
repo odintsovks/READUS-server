19737ff53c3d5d9a1eb4a8342a68a9768994b816
odintsovks <ksodintsov03@gmail.com>
2026-03-02 03:01:27 +0300

    flake: init

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "progarch";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in
+  {
+    devShell.${system} = pkgs.mkShellNoCC {
+      shellHook = ''
+        export JDTLS_CONFIG='cmd = {"${pkgs.jdt-language-server}/bin/jdtls"}'
+      '';
+      packages = with pkgs; [
+        javaPackages.compiler.temurin-bin.jdk-21
+        maven
+      ];
+    };
+  };
+}
